c6f8b1e52e07b8421cf984d6d956d80404b4bb70
Shaista_Daniya <85442187+ImShaistaDaniya@users.noreply.github.com>
2022-01-15 22:31:23 +0530

    Add files via upload

---
+++ DHT_blynk.ino
@@ -0,0 +1,41 @@
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <DHT.h>
+char auth[] = "7zgcCM2nSsGkwAGQHcbsNLhkF39OOgIq";
+char ssid[] = "ZTE-Fjeg5C";
+char pass[] = "q2fe9rr5";
+#define DHTPIN D3          // What digital pin we're connected to
+#define DHTTYPE DHT11     // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
+BlynkTimer timer;
+
+void sendSensor()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V5, h);
+  Blynk.virtualWrite(V6, t);
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+  dht.begin();
+  timer.setInterval(1000L, sendSensor);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+}
